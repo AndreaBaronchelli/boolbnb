50b6c24e460a53a752e78c044265d3dcf9f7ecbd
GioOli7 <gio07live@gmail.com>
2021-07-09 17:02:36 +0200

    add call seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call([
+            ApartmentsTableSeeder::class,
+            MessagesTableSeeder::class,
+            ServicesTableSeeder::class,
+            SponsorsTableSeeder::class,
+        ]);
     }
 }
