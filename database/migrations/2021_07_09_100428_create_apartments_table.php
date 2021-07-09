26d93164a70fb573adbf6264dfca85c19b8bb5b5
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-07-09 13:14:29 +0200

    add migrations/models/relations

---
+++ database/migrations/2021_07_09_100428_create_apartments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->string('title', 100);
+            $table->string('slug');
+            $table->tinyInteger('rooms')->unsigned();
+            $table->tinyInteger('beds')->unsigned()->nullable();
+            $table->tinyInteger('bathrooms')->unsigned()->nullable();
+            $table->smallInteger('square_meters')->unsigned();
+            $table->string('address');
+            $table->float('latitude', 8, 6);
+            $table->float('longitude', 9, 6);
+            $table->string('image')->nullable();
+            $table->boolean('visibility');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
