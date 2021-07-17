14f5a5dd25605de267953b6bd2f790fd0dbfbdc6
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-07-17 09:19:57 +0200

    add marker

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -13,6 +13,7 @@ class ApartmentController extends Controller
     //get apartment on frontend
 
     public function index(){
+
         // dd($address);
         $apartments = Apartment::all();
 
@@ -21,24 +22,25 @@ class ApartmentController extends Controller
     }
 
     //get post detail by slug
-    public function show($address){
-        // dump($address);
+    // public function show($address){
 
-        $response = Http::get("https://api.tomtom.com/search/2/search/${address}.json?radius=20000&key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML");
+    //     // dump($address);
+
+    //     // $response = Http::get("https://api.tomtom.com/search/2/search/${address}.json?radius=20000&key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML");
         
-        $posLat = $response->json()['results'][0]['position']['lat'];
-        $posLon = $response->json()['results'][0]['position']['lon'];
+    //     // $posLat = $response->json()['results'][0]['position']['lat'];
+    //     // $posLon = $response->json()['results'][0]['position']['lon'];
         
 
-        $response = Http::get("https://api.tomtom.com/search/2/geometrySearch/${address}.json?geometryList=%7B%22type%22%3A%22CIRCLE%22%2C%20%22position%22%3A%22${posLat}%2C%20${posLon}%22%2C%20%22radius%22%3A20000%7D&key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML");
+    //     // $response = Http::get("https://api.tomtom.com/search/2/geometrySearch/${address}.json?geometryList=%7B%22type%22%3A%22CIRCLE%22%2C%20%22position%22%3A%22${posLat}%2C%20${posLon}%22%2C%20%22radius%22%3A20000%7D&key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML");
 
-        //TESTARE
-        // $response = Http::get("https://api.tomtom.com/search/2/search/${address}.json?key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML&lat=${posLat}&lon=${posLon}&radius=20000&idxSet=Str");
+    //     //TESTARE
+    //     // $response = Http::get("https://api.tomtom.com/search/2/search/${address}.json?key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML&lat=${posLat}&lon=${posLon}&radius=20000&idxSet=Str");
         
-        dd($response->json());
-        // $post = Post::where('slug', $slug)->with(['category','tags'])->first();
-        // return response()->json($post); 
-    }
+    //     // dd($response->json());
+    //     // $post = Post::where('slug', $slug)->with(['category','tags'])->first();
+    //     // return response()->json($post); 
+    // }
 
 }
 
--- public/js/admin.js
+++ public/js/admin.js
@@ -37460,7 +37460,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Maurizio\Desktop\boolbnb\resources\js\admin.js */"./resources/js/admin.js");
+module.exports = __webpack_require__(/*! /Users/andrea/Desktop/Boolean/Esercizi/boolbnb/resources/js/admin.js */"./resources/js/admin.js");
 
 
 /***/ })
