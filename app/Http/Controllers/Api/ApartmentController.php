266220bc64946da8af796d9008df76af9ce20ee8
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-07-18 11:14:14 +0200

    search

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -22,25 +22,33 @@ class ApartmentController extends Controller
     }
 
     //get post detail by slug
-    // public function show($address){
+    public function show($address){
 
-    //     // dump($address);
+        // dump($address);
 
-    //     // $response = Http::get("https://api.tomtom.com/search/2/search/${address}.json?radius=20000&key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML");
+        $response = Http::get("https://api.tomtom.com/search/2/search/${address}.json?radius=20000&key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML");
         
-    //     // $posLat = $response->json()['results'][0]['position']['lat'];
-    //     // $posLon = $response->json()['results'][0]['position']['lon'];
+        $posLat = $response->json()['results'][0]['position']['lat'];
+        $posLon = $response->json()['results'][0]['position']['lon'];
         
+        $minLat = $posLat - 0.03;
+        $maxLat = $posLat + 0.03;
+        $minLon = $posLon - 0.03;
+        $maxLon = $posLon + 0.03;
 
-    //     // $response = Http::get("https://api.tomtom.com/search/2/geometrySearch/${address}.json?geometryList=%7B%22type%22%3A%22CIRCLE%22%2C%20%22position%22%3A%22${posLat}%2C%20${posLon}%22%2C%20%22radius%22%3A20000%7D&key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML");
+        $apartments = Apartment::all()->whereBetween('latitude', [$minLat, $maxLat], 'longitude', [$minLon, $maxLon]);
 
-    //     //TESTARE
-    //     // $response = Http::get("https://api.tomtom.com/search/2/search/${address}.json?key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML&lat=${posLat}&lon=${posLon}&radius=20000&idxSet=Str");
+        return response()->json($apartments);
+
+        // $response = Http::get("https://api.tomtom.com/search/2/geometrySearch/${address}.json?geometryList=%7B%22type%22%3A%22CIRCLE%22%2C%20%22position%22%3A%22${posLat}%2C%20${posLon}%22%2C%20%22radius%22%3A20000%7D&key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML");
+
+        //TESTARE
+        // $response = Http::get("https://api.tomtom.com/search/2/search/${address}.json?key=4j77acI2RkgcxaYW2waGQ74SEPwpmFML&lat=${posLat}&lon=${posLon}&radius=20000&idxSet=Str");
         
-    //     // dd($response->json());
-    //     // $post = Post::where('slug', $slug)->with(['category','tags'])->first();
-    //     // return response()->json($post); 
-    // }
+        // dd($response->json());
+        // $post = Post::where('slug', $slug)->with(['category','tags'])->first();
+        // return response()->json($post); 
+    }
 
 }
 
